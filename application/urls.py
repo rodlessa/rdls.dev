fd82f2631f54a1c5b24d6c7f1aba25a94525927e
unknown <Vonixx@LAPTOP-63QHAQPP.vonixx.local>
2023-05-29 21:10:10 -0300

    update files

--- application/urls.py
+++ application/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.anything),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
